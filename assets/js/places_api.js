e8dd9c4a8894718e59340458b1828cb013be79a7
Samuel Riveros <sam_riveros18@outlook.com>
2021-12-15 18:17:25 -0500

    attempt number 5

--- assets/js/places_api.js
+++ assets/js/places_api.js
@@ -1,10 +1,10 @@
 var autocomplete;
 
 function getAutocomplete(){
-    autocomplete = new google.maps.places.Autocomplete(document.getElementsById("enter_text"),
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById("enter_text"),
     {
         types:['establishment'],
         componentRestrictions:{'country': ['AU']}, 
         fields: ['place_id', 'geometry','name']
     });
-} 
+}
