0ff079126729e61a307317cae9a4a7fa06987928
Samuel Riveros <sam_riveros18@outlook.com>
2021-12-16 11:56:08 -0500

    combided each feature to work with the search bar and submit button.

--- assets/js/google_api.js
+++ assets/js/google_api.js
@@ -1,45 +1,52 @@
+//global values from geocode
+var lat1;
+var lng1;
 
+// inizilizes api
+function initApi(){
+    initAutocomplete();
+}
 // function for search bar
 var autocomplete;
 
-function getAutocomplete(){
+function initAutocomplete(){
     autocomplete = new google.maps.places.Autocomplete(document.getElementById("enter_text"),
     {
         types:['(cities)'],
-        // componentRestrictions:{'country':['AU']},
         fields: ['place_id','geometry','name']
         
     });
 
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
-
+// handels what would happen if a place wasn't entered correctly 
 function onPlaceChanged(){
     var place= autocomplete.getPlace();
 
     if(!place.geometry){
         document.getElementById('enter_text').placeholer="Enter Place";
+        return console.log("oh no enter a city");
     }
 
     else{
-        document.getElementById('details').innerHTML = place.name;
+        var locationForm = document.getElementById("submit-location");
+
+        locationForm.addEventListener('submit',geocode); 
+        document.getElementById('enter_text').innerHTML = place.name;
     }
 }
 
-// functions creates a new map
-var latitude= 33.7490;
-var longitude= -84.3880;
-var coords= latitude, longitude;
-
-function initMap(coords){
+// function populates a google map depending on user input
+function initMap(latitude, longitude){
 
     var options ={
-        zoom: 8,
+        zoom: 9,
         center: {lat:latitude, lng:longitude}
     }
 
     var map = new google.maps.Map(document.getElementById('map'),options);
 
+    // creates a marker for the location which user selected
     var marker = new google.maps.Marker({
         position:{lat:latitude, lng:longitude},
         map: map
@@ -47,20 +54,37 @@ function initMap(coords){
 
 }
 
-geocode();
+// geocode function that waits for search button to be clicked
+function geocode(e){
+    
+    //prevents submition form loading to server
+    e.preventDefault();
 
-function geocode(){
-    var location="22 Main st Boston MA";
+    // calls for geolocation API
+    var location=document.getElementById('enter_text').value;
     axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
         params:{
             address:location,
-            key:"AIzaSyBgGkZtN3w9l6EmmjSgcGjvbPME4ILKP0w"
+            key:"AIzaSyBdbdSYXsQnnq9X9E7IyEwweli047A9DCI"
         }
     })
     .then(function(response){
+
         console.log(response);
+
+        lat1 = (response.data.results[0].geometry.location.lat);
+        lng1 = (response.data.results[0].geometry.location.lng);
+    
+        initMap(lat1,lng1);
     })
     .catch(function(error){
         console.log(error);
     });
+
+    
 }
+
+
+
+
+
